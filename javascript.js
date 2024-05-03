972700188a737559b1d08ce3e81280386bf6b8ba
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-05-03 11:47:10 -0400

    Fussing over mobile view and tweaks/fixes to mobile-sidebar menu

--- javascript.js
+++ javascript.js
@@ -1,27 +1,30 @@
-const hamburgerBtn = document.querySelector("#hamburger-btn");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
 const headerSection = document.querySelector("header");
 const mainContent = document.querySelector("main-content");
 const footerSection = document.querySelector("footer");
 
 const hiddenMobileSidebar = document.querySelector(".hidden-mobile-sidebar");
 
-const mobileSidebar = document.querySelector("mobile-sidebar");
+const mobileSidebarBtn = document.querySelector(".mobile-sidebar-btn");
 
 hamburgerBtn.addEventListener("click", showSidebarMenu);
-mobileSidebar.addEventListener("click", hideSidebarMenu);
+mobileSidebarBtn.addEventListener("click", hideSidebarMenu);
 
-function showSidebarMenu() {
-    console.log("Test!!!!");
-    // hiddenMobileSidebar.style.display = "grid";
+
+function showSidebarMenu(event) {
+    console.log("Test1: mobile-sidebar is shown.");
+    event.preventDefault(); 
+    hiddenMobileSidebar.style.display = "grid";
     headerSection.style.display = "none";
     mainContent.style.display = "none";
     footerSection.style.display = "none";
 }
 
 function hideSidebarMenu() {
-    console.log("Test2!!!!");
+    console.log("Test2: mobile-sidebar is hidden.");
     hiddenMobileSidebar.style.display = "none";
     headerSection.style.display = "flex";
     mainContent.style.display = "grid";
     footerSection.style.display = "flex";
+
 }
