bbd97f1e8ecd1bdebdce41e334ea24bf0548aeb9
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-05-04 12:18:39 -0400

    Repositioned hamburger-menu button

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
+const hamburgerContainer = document.querySelector("hamburger-container");
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const headerSection = document.querySelector("header");
 const mainContent = document.querySelector("main-content");
 const footerSection = document.querySelector("footer");
 
 const hiddenMobileSidebar = document.querySelector(".hidden-mobile-sidebar");
-
 const mobileSidebarBtn = document.querySelectorAll(".mobile-sidebar-btn");
 
 hamburgerBtn.addEventListener("click", showSidebarMenu);
@@ -12,11 +12,11 @@ mobileSidebarBtn.forEach(function(button) {
 button.addEventListener("click", hideSidebarMenu);
 });
 
-
 function showSidebarMenu(event) {
     console.log("Test1: mobile-sidebar is shown.");
     event.preventDefault(); 
     hiddenMobileSidebar.style.display = "grid";
+    hamburgerContainer.style.display = "none";
     headerSection.style.display = "none";
     mainContent.style.display = "none";
     footerSection.style.display = "none";
@@ -25,6 +25,7 @@ function showSidebarMenu(event) {
 function hideSidebarMenu() {
     console.log("Test2: mobile-sidebar is hidden.");
     hiddenMobileSidebar.style.display = "none";
+    hamburgerContainer.style.display = "block";
     headerSection.style.display = "flex";
     mainContent.style.display = "grid";
     footerSection.style.display = "flex";
